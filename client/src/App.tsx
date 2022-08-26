9291661cb856ff868e61fd2b2e51fc5c8c13e9d6
brpinto <brpinto@student.42.fr>
2022-08-26 02:15:45 +0200

    add main container to app and heading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import styled from "styled-components"
+
 function App() {
   return (
-    <h1>Prices Visualization</h1>
+    <>
+      <MainContainer>
+        <Heading>
+          Prices Visualization
+        </Heading>
+      </MainContainer>
+    </>
   );
 }
+
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`
+
+const Heading = styled.h1`
+  
+`
 
 export default App;
